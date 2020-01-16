726df7d5d598fff77a51bc355ab44ee8dc9b7169
freireguilherme <guilhermemagalhaesfreire22@gmail.com>
2020-01-16 12:25:16 -0300

    exercício 7

---
+++ program07.js
@@ -0,0 +1,10 @@
+var http = require('http')
+var https = require('https')
+
+var url = process.argv[2]
+
+http.get(url, function (response) {
+    response.setEncoding('utf8')                  //string ao inves de Buffer
+    response.on('data', console.log)              //manda 'data' para console.log
+    response.on('error', console.error)           //manda 'error' para console.error
+});
