ab24416f2c715401a9f47788756101c28b8c5e9a
freireguilherme <guilhermemagalhaesfreire22@gmail.com>
2020-01-17 13:05:47 -0300

    Some new comments and writeHead

--- program11.js
+++ program11.js
@@ -1,13 +1,14 @@
 const http   = require('http');
 const fs     = require('fs');
 
-let port     = process.argv[2];
-let file     = process.argv[3];
+let port     = process.argv[2];                                 //porta a ser ouvida, passada por parametro
+let file     = process.argv[3];                                 //localização do arquivo
 
-const server = http.createServer((req, res) => {
-    fs.createReadStream(file).pipe(res)
+const server = http.createServer((req, res) => {                //cria o HTTP server
+    res.writeHead (200, {   'content-type': 'text/plain'    })  //escreve no cabeçalho o success code 200 e JSON com o tipo do conteudo
+    fs.createReadStream(file).pipe(res)                         //passa o conteudo para memoria e entao encapsula para 'res'
 })
 
-server.listen(+port, () => {
+server.listen(+port, () => {                                    //listen a porta e quando a conexão é feita, mostra no cosole.log qual a porta
     console.log('Server listening on http://localhost: %s', port)
 })
