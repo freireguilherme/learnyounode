c52b1eb6c13c0a9c21edd1d313b3be28e805287f
freireguilherme <guilhermemagalhaesfreire22@gmail.com>
2020-01-17 12:57:52 -0300

    Some new comments

--- program09.js
+++ program09.js
@@ -10,18 +10,18 @@ function imprimir () {                          //imprime
 
 function httpGet (index) {
   http.get(process.argv[2 + index], (resposta) => {
-    resposta.pipe(bl( (err, data) => {              //metodo pipe para encapsular o data no buffer line  
-      if (err)                                      // para que todo conteudo de data seja mostrado
-        return console.error("Houve um erro:" + err);
+    resposta.pipe(bl( (err, data) => {              //metodo pipe para encapsular o data no buffer line para que todo conteudo de data seja mostrado 
+        if (err)                                     
+            return console.error("Houve um erro:" + err);
 
         resultados[index] = data.toString()         //apenas a data[2] sera armazenada em resutados[2], etc
-      contador++;
+        contador++;
 
-      if (contador == 3)                            //chama a funçao apenas ao fim dos tres pedidos
-        imprimir()
+        if (contador == 3)                            //chama a funçao apenas ao fim dos tres pedidos
+            imprimir()
     }))
   })
 }
 
-for (var i = 0; i < 3; i++)                         
-  httpGet(i)
+for (var i = 0; i < 3; i++)
+    httpGet(i)
--- program10.js
+++ program10.js
@@ -1,19 +1,19 @@
 const net = require('net');
 
 function completarZero(i){
-    return ( i < 10? '0' : '') + i;             //se o valor for na casa das unidades (menor que 10), precisa de um zero na frente
+    return ( i < 10? '0' : '') + i;                 // se o valor for na casa das unidades (menor que 10), precisa de um zero na frente
 }
 
-function dataAgora() {                          //funcção que retorna a data atual
+function dataAgora() {                              // funcção que retorna a data atual
     let d  = new Date();
-    return d.getFullYear() + '-'                   // ano
+    return d.getFullYear() + '-'                    // ano
         + completarZero(d.getMonth() + 1) + '-'     // mês
         + completarZero(d.getDate()) + ' '          // dia
-        + completarZero(d.getHours()) + ':'         //hora
-        + completarZero(d.getMinutes());            //minutos
+        + completarZero(d.getHours()) + ':'         // hora
+        + completarZero(d.getMinutes());            // minutos
 }
 
-const server =  net.createServer((socket)=> {       //criou o server para e escreveu dataAgora no socket
+const server =  net.createServer((socket)=> {       //criou o TCP server para e escreveu dataAgora no socket
     socket.end(dataAgora() + '\n');
 })
 server.listen(Number(process.argv[2]));             //ouvindo a porta passanda via argv
